48d55c3f00a00bab0f8de21ed61d9fc7358c83dc
James Dusky <jamesdusky@dustytavern.com>
2017-12-14 14:50:43 -0500

    mastercss

--- web/index.php
+++ web/index.php
@@ -2,6 +2,7 @@
 <head>
   <title>Project Comet</title>
   <link rel="stylesheet" href="https://github.com/Aliman999/Blazar/blob/master/CSS/themes.css">
+  <link rel="stylesheet" href="/css/master.css">
 
 </head>
 <body>
