33841f12b2dd8c04994d1e81cfe1948878b55e1b
James Dusky <jamesdusky@dustytavern.com>
2017-12-14 18:24:02 -0500

    v1

--- web/index.php
+++ web/index.php
@@ -4,13 +4,38 @@
   <link rel="stylesheet" href="/css/themes.css">
   <link rel="stylesheet" href="/css/shader.css">
   <link rel="stylesheet" href="/css/master.css">
+  <link rel="stylesheet" href="/css/animations.css">
 
-  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,900" rel="stylesheet">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+
+    });
+</script>
 </head>
 <body>
   <div class="md-whiteSpace md-firstSpace md-primary">
 
+    <div class="md-header">
+
+    <img class="md-logo" src="/src/Starico.png" alt="">
+
+    <h1 class="md-title">PROJECT COMET</h1>
+
+    </div>
+
+    <div class="md-content md-whiteSpace md-secondary">
+    </div>
   </div>
 </body>
+
+<?php
+
+
+
+?>
 
 </html>
