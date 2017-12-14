62964cfc4873d2bc9c8332cf304edda2b993e98c
James Dusky <jamesdusky@dustytavern.com>
2017-12-14 14:49:47 -0500

    v001

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+  <title>Project Comet</title>
+  <link rel="stylesheet" href="https://github.com/Aliman999/Blazar/blob/master/CSS/themes.css">
+
+</head>
+<body>
+  <div class="md-primary">
+
+  </div>
+</body>
+
+</html>
