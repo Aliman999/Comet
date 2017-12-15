91b6edfad80f9419e54c0023ad1679d4cc54b327
James Dusky <jamesdusky@dustytavern.com>
2017-12-15 00:14:51 -0500

    v02

---
+++ web/dashboard/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+  <title>Project Comet</title>
+  <link rel="stylesheet" href="/css/master.css">
+  <link rel="stylesheet" href="/css/navbar.css">
+</head>
+<body>
+<?php include '../php/navbar.php' ?>
+</body>
+</html>
---
+++ web/php/navbar.php
@@ -0,0 +1,6 @@
+<div class="nb-container">
+
+  <div class="nb-menu">
+    <input type="button" name="" value="">
+  </div>
+</div>
